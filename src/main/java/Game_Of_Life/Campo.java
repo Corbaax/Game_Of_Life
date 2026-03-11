4929a7ec9b3a2a5fd1a04750baef696508a2e51a
Luca Corbetta <luke.corbetta@gmail.com>
2026-03-11 16:15:42 +0100

    Implementato Codice per testCreation.

--- src/main/java/Game_Of_Life/Campo.java
+++ src/main/java/Game_Of_Life/Campo.java
@@ -3,14 +3,23 @@ package Game_Of_Life;
 import static Game_Of_Life.Status.*;
 
 public class Campo {
+    private Status[][] matrix;
+
     public Campo(int row, int column) {
+        this.matrix = new Status[row][column];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                matrix[i][j] = Dead;
+            }
+        }
     }
 
     public void makeLife(int row, int column) {
+        matrix[row][column] = Alive;
     }
 
     public Status cellAt(int row, int column) {
-        return Dead;
+        return matrix[row][column];
     }
 
     public void tick() {
